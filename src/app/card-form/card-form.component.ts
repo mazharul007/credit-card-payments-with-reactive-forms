d9fe64fa915929f7249753a09ca57ca9eddb204c
mazharul007 <mohammad.mazharul.hoque7@gmail.com>
2022-04-18 12:25:23 +0600

    validayion added

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 @Component({
   selector: 'app-card-form',
   templateUrl: './card-form.component.html',
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardFormComponent implements OnInit {
 
-  constructor() { }
+
+  // This cardFrom property represents our whole form
+  cardForm = new FormGroup({
+     name: new FormControl('', [
+       Validators.required,
+       Validators.minLength(5)]
+    )
+  })
+  constructor() { 
+    console.log(this.cardForm.controls.name)
+  }
 
   ngOnInit(): void {
   }
--- src/app/card-form/card-form.module.ts
+++ src/app/card-form/card-form.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardFormComponent } from './card-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [ CardFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule 
   ],
   exports:[
     CardFormComponent
